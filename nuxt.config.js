78182cf9a3170f1782c486eb4243102e04efa339
Shafqat Ali <shafqatalirafaqat@gmail.com>
2021-07-23 09:08:59 +0500

    7-23-2021

--- nuxt.config.js
+++ nuxt.config.js
@@ -22,6 +22,7 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
+
   css: ["~assets/sass/main.scss"],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
@@ -33,6 +34,7 @@ export default {
   //routes
 
   // Auto import components: https://go.nuxtjs.dev/config-components
+  
   components: true,
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
